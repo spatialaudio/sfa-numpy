1bc8bcaea9810330bf9190c4eb86785034af51e0
Sascha Spors <Sascha.Spors@uni-rostock.de>
2017-09-13 14:32:23 +0200

    Fixed examples

--- examples/modal_beamforming_open_array.py
+++ examples/modal_beamforming_open_array.py
@@ -10,14 +10,15 @@ import micarray
 N = 20  # order of modal beamformer/microphone array
 pw_angle = (np.pi, np.pi/2)  # incidence angle of plane wave
 azi_pwd = np.linspace(0, 2*np.pi, 91, endpoint=False)  # angles for plane wave decomposition
-kr = np.linspace(0.1, 20, 100)  # wavenumber-radius vector
+k = np.linspace(0.1, 20, 100)  # wavenumber vector
+r = 1  # radius of array
 
 
 def dot_product_sph(v, u):
     # evaluate dot-product between u and v in spherical coordinates
-    return (np.cos(v[0])*np.sin(v[1])*np.cos(u[0])*np.sin(u[1])
-            + np.sin(v[0])*np.sin(v[1])*np.sin(u[0])*np.sin(u[1])
-            + np.cos(v[1])*np.cos(u[1]))
+    return (np.cos(v[0])*np.sin(v[1])*np.cos(u[0])*np.sin(u[1]) +
+            np.sin(v[0])*np.sin(v[1])*np.sin(u[0])*np.sin(u[1]) +
+            np.cos(v[1])*np.cos(u[1]))
 
 
 # get quadrature grid (microphone positions) of order N
@@ -27,12 +28,12 @@ Y_p = micarray.modal.angular.sht_matrix(N, azi, elev, weights)
 # get spherical harmonics matrix for a source ensemble of azimuthal plane waves
 Y_q = micarray.modal.angular.sht_matrix(N, azi_pwd, np.pi/2)
 # get radial filters
-bn = micarray.modal.radial.spherical(N, kr, setup='open', plane_wave=True)
+bn = micarray.modal.radial.spherical_pw(N, k, r, setup='open')
 dn, _ = micarray.modal.radial.regularize(1/bn, 100, 'softclip')
 D = micarray.modal.radial.diagonal_mode_mat(dn)
 
 # compute microphone signals for an incident broad-band plane wave
-p = np.exp(1j * kr[:, np.newaxis] * dot_product_sph((azi, elev), pw_angle))
+p = np.exp(1j * k[:, np.newaxis]*r * dot_product_sph((azi, elev), pw_angle))
 # compute the plane wave dcomposition
 A_pwd = np.matmul(np.matmul(np.conj(Y_q.T), D), Y_p)
 q_pwd = np.squeeze(np.matmul(A_pwd, np.expand_dims(p, 2)))
@@ -40,7 +41,7 @@ q_pwd_t = np.fft.fftshift(np.fft.irfft(q_pwd, axis=0), axes=0)
 
 # visualize plane wave decomposition (aka beampattern)
 plt.figure()
-plt.pcolormesh(kr, azi_pwd/np.pi, 20*np.log10(np.abs(q_pwd.T)), vmin=-10)
+plt.pcolormesh(k, azi_pwd/np.pi, 20*np.log10(np.abs(q_pwd.T)), vmin=-40)
 plt.colorbar()
 plt.xlabel(r'$kr$')
 plt.ylabel(r'$\phi / \pi$')
@@ -48,7 +49,7 @@ plt.title('Plane wave docomposition by modal beamformer (frequency domain)')
 plt.savefig('modal_open_beamformer_pwd_fd.png')
 
 plt.figure()
-plt.pcolormesh(range(2*len(kr)-2), azi_pwd/np.pi, 20*np.log10(np.abs(q_pwd_t.T)), vmin=-10)
+plt.pcolormesh(range(2*len(k)-2), azi_pwd/np.pi, 20*np.log10(np.abs(q_pwd_t.T)), vmin=-40)
 plt.colorbar()
 plt.ylabel(r'$\phi / \pi$')
 plt.title('Plane wave docomposition by modal beamformer (time domain)')
--- examples/modal_beamforming_rigid_array.py
+++ examples/modal_beamforming_rigid_array.py
@@ -10,14 +10,15 @@ import micarray
 N = 20  # order of modal beamformer/microphone array
 azi_pw = np.pi  # incidence angle of plane wave
 azi_pwd = np.linspace(0, 2*np.pi, 91, endpoint=False)  # angles for plane wave decomposition
-kr = np.linspace(0.1, 20, 100)  # wavenumber-radius vector
+k = np.linspace(0.1, 20, 100)  # wavenumber
+r = 1  # radius of array
 
 
 # get quadrature grid (microphone positions) of order N
 azi, elev, weights = micarray.modal.angular.grid_gauss(N)
 
 # pressure on the surface of a rigid sphere for an incident plane wave
-bn = micarray.modal.radial.spherical(N, kr, setup='rigid', plane_wave=True)
+bn = micarray.modal.radial.spherical_pw(N, k, r, setup='rigid')
 D = micarray.modal.radial.diagonal_mode_mat(bn)
 Y_p = micarray.modal.angular.sht_matrix(N, azi, elev)
 Y_pw = micarray.modal.angular.sht_matrix(N, azi_pw, np.pi/2)
@@ -30,7 +31,7 @@ Y_p = micarray.modal.angular.sht_matrix(N, azi, elev, weights)
 azi_pwd = np.linspace(0, 2*np.pi, 91, endpoint=False)
 Y_q = micarray.modal.angular.sht_matrix(N, azi_pwd, np.pi/2)
 # get radial filters
-bn = micarray.modal.radial.spherical(N, kr, setup='rigid', plane_wave=True)
+bn = micarray.modal.radial.spherical_pw(N, k, r, setup='rigid')
 dn, _ = micarray.modal.radial.regularize(1/bn, 100, 'softclip')
 D = micarray.modal.radial.diagonal_mode_mat(dn)
 # compute the PWD
@@ -41,7 +42,7 @@ q_mb_t = np.fft.fftshift(np.fft.irfft(q_mb, axis=0), axes=0)
 
 # visualize plane wave decomposition (aka beampattern)
 plt.figure()
-plt.pcolormesh(kr, azi_pwd/np.pi, 20*np.log10(np.abs(q_mb.T)), vmin=-40)
+plt.pcolormesh(k*r, azi_pwd/np.pi, 20*np.log10(np.abs(q_mb.T)), vmin=-40)
 plt.colorbar()
 plt.xlabel(r'$kr$')
 plt.ylabel(r'$\phi / \pi$')
@@ -49,7 +50,7 @@ plt.title('Plane wave docomposition by modal beamformer (frequency domain)')
 plt.savefig('modal_rigid_beamformer_pwd_fd.png')
 
 plt.figure()
-plt.pcolormesh(range(2*len(kr)-2), azi_pwd/np.pi, 20*np.log10(np.abs(q_mb_t.T)), vmin=-40)
+plt.pcolormesh(range(2*len(k)-2), azi_pwd/np.pi, 20*np.log10(np.abs(q_mb_t.T)), vmin=-40)
 plt.colorbar()
 plt.ylabel(r'$\phi / \pi$')
 plt.title('Plane wave docomposition by modal beamformer (time domain)')
