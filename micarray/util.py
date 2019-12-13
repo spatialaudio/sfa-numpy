0aedc37073c37cb6ccd72d4c998ac13295cbd418
Nara Hahn <n.hahn343@gmail.com>
2019-12-13 13:10:33 +0100

    Update radial.py and util.py

--- micarray/util.py
+++ micarray/util.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import linalg
+from scipy.special import eval_legendre as legendre
 
 
 def norm_of_columns(A, p=2):
@@ -93,7 +94,7 @@ def matdiagmul(A, b):
     return C
 
 
-def db(x, power=False):
+def db(x, *, power=False):
     """Convert *x* to decibel.
 
     Parameters
@@ -106,4 +107,61 @@ def db(x, power=False):
 
     """
     with np.errstate(divide='ignore'):
-        return 10 if power else 20 * np.log10(np.abs(x))
+        return (10 if power else 20) * np.log10(np.abs(x))
+
+
+def double_factorial(n):
+    """Double factorial"""
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return n * double_factorial(n - 2)
+
+
+def maxre_sph(N):
+    """max-reE modal weight for spherical harmonics expansion.
+
+    Parameter
+    ---------
+    N : int
+        Highest spherical harmonic order (Ambisonic order).
+
+    """
+    theta = np.deg2rad(137.9 / (N + 1.52))
+    return legendre(np.arange(N + 1), np.cos(theta))
+
+
+def point_spread(N, phi, modal_weight=maxre_sph, equalization='omni'):
+    """Directional response of a given modal weight function and
+    equalization scheme.
+
+    Parameters
+    ----------
+    N : int
+        Highest spherical harmonic order (Ambisonic order).
+    phi : array_like
+        Angular distance from the main axis in radian.
+    modal_weight : callable, optional
+        Modal weighting function.
+    equalization : {'omni', 'diffuse', 'free'}, optional
+        Equalization scheme.
+
+    """
+    a = modal_weight(N)
+    if equalization == 'omni':
+        pass
+    elif equalization == 'diffuse':
+        a *= 1 / modal_norm(a, ord=2)
+    elif equalization == 'free':
+        a *= 1 / modal_norm(a, ord=1)
+    return np.stack([(2*n+1) * a[n] * legendre(n, np.cos(phi))
+                     for n in range(N+1)])
+
+
+def modal_norm(a, ord=2):
+    """Norm of the coefficients in the spherical harmonics domain.
+    """
+    num_degree = 2 * np.arange(a.shape[-1]) + 1
+    return np.sum(num_degree * a**ord, axis=-1)**(1/ord)
